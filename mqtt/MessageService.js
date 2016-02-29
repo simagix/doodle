8cd0f23f2ba79fa35f35f1b3bd3bcd6770b7b574
Kuei-chun Chen <simagix@gmail.com>
2016-02-29 13:30:56 -0500

    Add mqtt broker connected message

--- mqtt/MessageService.js
+++ mqtt/MessageService.js
@@ -1,24 +1,22 @@
-var mqtt    = require('mqtt');
+var mqtt = require('mqtt');
 console.log('connecting to ' + process.env.MQTT_BROKER);
-var client  = mqtt.connect(process.env.MQTT_BROKER);
+var client = mqtt.connect(process.env.MQTT_BROKER);
 var queue_simagix = 'simagix';
 var queue_simagix_color = 'simagix_color';
 
 client.on('connect', function () {
-    if(process.env.ENV == 'dev') {
-        console.log('connected to ' + process.env.MQTT_BROKER);
-    }
+    console.log('connected to ' + process.env.MQTT_BROKER);
 });
 
-exports.sendMessage = function(data, cb) {
-    if(process.env.ENV == 'dev') {
+exports.sendMessage = function (data, cb) {
+    if (process.env.ENV == 'dev') {
         console.log('publishing ' + data);
     }
     client.publish(queue_simagix, data);
 }
 
-exports.sendColorValuesMessage = function(data, cb) {
-    if(process.env.ENV == 'dev') {
+exports.sendColorValuesMessage = function (data, cb) {
+    if (process.env.ENV == 'dev') {
         console.log('publishing ' + data);
     }
     client.publish(queue_simagix_color, data);
